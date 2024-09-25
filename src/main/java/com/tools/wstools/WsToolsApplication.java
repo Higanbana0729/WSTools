03b1c5c68b4e222a248d3ece9a1578a89106cd02
zp <591369879@qq.com>
2024-09-25 22:59:54 +0800

    first commit

---
+++ src/main/java/com/tools/wstools/WsToolsApplication.java
@@ -0,0 +1,13 @@
+package com.tools.wstools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tools/wstools/WsToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.wstools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
