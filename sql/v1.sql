cecdd56df337d49a6be34a1687ec700ac458c72e
zp <591369879@qq.com>
2024-09-29 17:31:35 +0800

    feat(config) 添加数据库配置和相关依赖

---
+++ sql/v1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `user` (
+                        `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+                        `name` VARCHAR(50) NOT NULL,
+                        `age` INT(11) NOT NULL,
+                        `email` VARCHAR(50) NOT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+
+
+
